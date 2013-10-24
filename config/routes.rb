ad87b03098db05df03e6b3a4a7353db7c3925991
Victor Ching <victorching@Victors-MacBook-Air-2.local>
2013-10-24 18:53:03 +0900

    add orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Doume::Application.routes.draw do
+  root 'orders#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
